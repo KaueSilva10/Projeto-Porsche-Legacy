05f50a6a92a0a4793407f325379387d6a634370e
Kauê <143094957+KaueSilva10@users.noreply.github.com>
2026-04-25 16:19:08 -0300

    últimos ajustes na página incial

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     let header = document.querySelector('.header');
-    if (window.scrollY > 550) {
+    if (window.scrollY > 615) {
         header.classList.remove('transparente');
     } else {
         header.classList.add('transparente');
